b4062b22b71392dbf134130df0a664c9af1637bb
Romain BERNAZ <bernazromain@gmail.com>
2021-02-08 20:17:57 +0100

    ejakeajzej

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import config from './config.js'
 import dotenv from 'dotenv';
+import * as path from 'path';
 import mongoose from 'mongoose'
 import userRoute from './routes/userRoute.js'
 import productRoute from './routes/productRoute.js';
@@ -27,6 +28,9 @@ app.use('/api', uploadRoute)
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('./frontend/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname,'frontend', 'build', 'index.html'));
+    })
 }
 
 
