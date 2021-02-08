acf1476d7ba1fd991fcfe929e6d22551c420e84c
Romain BERNAZ <bernazromain@gmail.com>
2021-02-08 20:08:51 +0100

    testetet

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use('/api', pageRoute)
 app.use('/api', uploadRoute)
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('frontend/build'))
+    app.use(express.static('./frontend/build'))
 }
 
 
