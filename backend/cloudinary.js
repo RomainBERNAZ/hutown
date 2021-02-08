5ac4aa7cd0b07806902a14c0c279c495074a5967
Romain BERNAZ <bernazromain@gmail.com>
2021-02-08 14:44:43 +0100

    tetattaezt

--- backend/cloudinary.js
+++ backend/cloudinary.js
@@ -1,13 +1,11 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-import cloudinary from 'cloudinary';
-
-const cl = new cloudinary.v2;
-cl.config({
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
     cloud_name: 'drefurx4l',
     api_key: '394767766592868',
     api_secret: '1zTfp--_hcBGyVjbOJx-u2ZO8Y8'
 });
 
-export default cl;
+export default cloudinary;
