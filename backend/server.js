1467260356ed9fb4111238c340ba75e5eea54103
Romain BERNAZ <bernazromain@gmail.com>
2021-02-09 11:40:38 +0100

    azeaeze

--- backend/server.js
+++ backend/server.js
@@ -30,9 +30,9 @@ app.use('/api', pageRoute)
 app.use('/api', uploadRoute)
     
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname,'frontend/build')))
+    app.use(express.static('frontend/build'))
     app.get('/*', (req, res) => {
-        res.sendFile(path.join(__dirname,'frontend', 'build', 'index.html'));
+        res.sendFile('frontend', 'build', 'index.html');
     })
 }
 
