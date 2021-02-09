bb608fb22a552bf35b9134166259200e6c071c36
Romain BERNAZ <bernazromain@gmail.com>
2021-02-09 12:14:04 +0100

    jkzerjhzehkr

--- backend/server.js
+++ backend/server.js
@@ -31,8 +31,9 @@ app.use('/api', uploadRoute)
       
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('frontend/build'))
+    console.log('LAAAAAAAAAAA', __dirname);
     app.get('/*', (req, res) => {
-        res.sendFile(path.join('../'+__dirname +'/frontend/build/index.html'));
+        res.sendFile('/app/frontend/build/index.html');
     })
 }
 
