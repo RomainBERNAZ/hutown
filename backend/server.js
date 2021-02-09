9e55a4b6f4b9663e4fadff779ce274fb1449fc35
Romain BERNAZ <bernazromain@gmail.com>
2021-02-09 11:28:19 +0100

    jzehakjeh

--- backend/server.js
+++ backend/server.js
@@ -30,9 +30,9 @@ app.use('/api', pageRoute)
 app.use('/api', uploadRoute)
     
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, 'frontend','build')))
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname,'frontend', 'build', 'index.html'));
+    app.use(express.static(path.join(__dirname,'build')))
+    app.get('/*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
     })
 }
 
