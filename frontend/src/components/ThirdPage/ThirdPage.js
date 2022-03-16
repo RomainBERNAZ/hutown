97a57d61741006b0f3556324d254227978b6bfe3
Romain Bernaz <45367271+RomainBERNAZ@users.noreply.github.com>
2022-03-16 20:12:33 +0100

    olzekjrlmkzerjzlmk

--- frontend/src/components/ThirdPage/ThirdPage.js
+++ frontend/src/components/ThirdPage/ThirdPage.js
@@ -26,7 +26,7 @@ const ThirdPage = () => {
             <div className="picturesUpload">
             { products && 
             <ArtistMainPhotos 
-                artist="Hutown"
+                artist="JEANNE"
                 products={products}
                 id="deuxiemeArtiste"
                 test={pages}
